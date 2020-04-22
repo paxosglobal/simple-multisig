b6776fd2037394686330bc74722d2f828717c5a3
Brent <bmperrea@gmail.com>
2020-04-22 10:02:33 -0500

    Add owners view function to get full ownersArr (#3)
    * Add owners view function to get full ownersArr
    
    * Update README.md

--- truffle.js
+++ truffle.js
@@ -5,5 +5,16 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  compilers: {
+    solc: {
+      version: "v0.4.24+commit.e67f0147" // Freeze solidity version
+    }
   }
 };
